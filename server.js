2aaec864475bcae123bb3380ae9d45438a2502cc
Daniel Kim <kyh8725@hotmail.com>
2020-08-18 20:57:40 -0700

    proxy

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
   // add other server routes to path array
-  app.use(proxy(["/"], { target: "http://localhost:8000" }));
+  app.use(proxy(["/routes/api/videos"], { target: "http://localhost:8000" }));
 };
 
 const port = 8000;
