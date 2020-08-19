66b97a34bf98a3bdc0420aaa49ba0a7d83454ee8
Daniel Kim <kyh8725@hotmail.com>
2020-08-18 21:39:49 -0700

    public

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+const videoRoute = require("./routes/api/videos");
+app.use("/", videoRoute);
 
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
@@ -33,9 +36,6 @@ if (process.env.NODE_ENV === "production") {
 //   },
 // };
 // app.use(cors(corsOptions));
-
-const videoRoute = require("./routes/api/videos");
-app.use("/", videoRoute);
 
 const port = 8000;
 app.listen(process.env.PORT || port, () => {
