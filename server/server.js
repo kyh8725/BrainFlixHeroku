98a55286772ae34d79763002b82fce3158800bcc
Daniel Kim <kyh8725@hotmail.com>
2020-09-11 12:49:42 -0700

    node modules reinstalled scripts rewritten

--- server/server.js
+++ server/server.js
@@ -8,6 +8,15 @@ app.use(express.static("public"));
 const videoRoute = require("./routes/api/videos");
 app.use("/", videoRoute);
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
