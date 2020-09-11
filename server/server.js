52c62d2d01851f2e3a05807ff89c8be9f4133c44
Daniel Kim <kyh8725@hotmail.com>
2020-09-11 13:15:44 -0700

    scripts

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static("public"));
 const videoRoute = require("./routes/api/videos");
+
 app.use("/", videoRoute);
+
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
@@ -17,7 +21,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
